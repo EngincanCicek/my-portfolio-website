cca25bbd39f8516c8b995feaa5cda6bc1dc1b4c7
Engincan Çiçek <mstrengincan22tr@gmail.com>
2025-02-16 04:37:38 +0300

    Profile Image Added and New Style Trying

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
         <div class="container">
             <h1>Hi, I'm Engincan 👋</h1>
             <p>Software Developer | Mobile Apps | Game Dev | AI & IT Solutions</p>
+            <img src="assets/img/profile.jpg" alt="Engincan Çiçek" class="profile-image">
+            <p> </p> <!-- Just for coloum display -->
             <a href="#about" class="btn">Learn More</a>
         </div>
     </section>
