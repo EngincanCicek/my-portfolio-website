28f8e9026f30c66ae3ab476a5752e94809b30eae
Engincan Çiçek <mstrengincan22tr@gmail.com>
2025-02-17 21:11:25 +0300

    Project Updated
    And a unnecessary image deleted.

--- projects.php
+++ projects.php
@@ -3,16 +3,53 @@
         <h2>Projects</h2>
         <div class="projects-container">
             <div class="card">
-                <h4>Statecraft: Corrupted Democracy</h4>
-                <p>A strategy game focused on political simulation, where players influence national stability and party dynamics.</p>
+                <a href="https://github.com/EngincanCicek/OUABootcampFlutter11" target="_blank">
+                    <h4>Muse - Digital Art Gallery</h4>
+                    <p>A Flutter-based app developed during a Google-backed bootcamp, showcasing digital art collections.</p>
+                </a>
             </div>
             <div class="card">
-                <h4>AI Damage Assessment App</h4>
-                <p>AI-powered app built with Flutter & OpenCV, providing real-time earthquake damage assessment.</p>
+                <a href="https://github.com/EngincanCicek/my-portfolio-website" target="_blank">
+                    <h4>My Portfolio Website</h4>
+                    <p>A personal portfolio showcasing my projects, skills, and experience, built with PHP, JS, and CSS.</p>
+                </a>
             </div>
             <div class="card">
-                <h4>PlanIdea</h4>
-                <p>An automated study planner using AI algorithms to help students optimize their learning schedule.</p>
+                <a href="https://store.steampowered.com/app/2838620/Statecraft_Corrupted_Democracy/" target="_blank">
+                    <h4>Statecraft: Corrupted Democracy</h4>
+                    <p>A political strategy game where players manage a party, balance interest groups, and make impactful decisions. Made with Unity C#</p>
+                </a>
+            </div>
+            <div class="card">
+                <a href="https://medium.com/@engincancicek/faster-earthquake-damage-detection-api-integration-and-evolutionary-optimization-for-real-world-db76087bee99" target="_blank">
+                    <h4>AI Damage Assessment App</h4>
+                    <p>A technical article on API integration and evolutionary optimization for post-earthquake damage assessment. 
+                        Using U-Net and OpenCV, the model swiftly classifies building damage. A Flutter app links to the AI, analyzing images and delivering real-time results.</p>
+                </a>
+            </div>
+            <div class="card">
+                <a href="https://github.com/EngincanCicek/Vote-Everything" target="_blank">
+                    <h4>Vote Everything</h4>
+                    <p>A Java & Firebase-based mobile app allowing users to create polls and vote on various topics.</p>
+                </a>
+            </div>
+            <div class="card">
+                <a href="https://github.com/EngincanCicek/PlanIdea-Website" target="_blank">
+                    <h4>PlanIdea Website</h4>
+                    <p>An automated study planner designed to help students prepare for exams more efficiently. Developed using Java/Kotlin, SQLite, and Firebase, it creates personalized study schedules based on user needs.</p>
+                </a>
+            </div>
+            <div class="card">
+                <a href="https://github.com/EngincanCicek/LeetCode-HackerRank-Solutions" target="_blank">
+                    <h4>LeetCode & HackerRank Solutions</h4>
+                    <p>A collection of algorithm and data structure solutions in Java, Python, and Kotlin.</p>
+                </a>
+            </div>
+            <div class="card">
+                <a href="https://github.com/EngincanCicek/OpenCV_Collection" target="_blank">
+                    <h4>OpenCV Collection</h4>
+                    <p>A set of computer vision projects using OpenCV for edge detection, object recognition, and segmentation.</p>
+                </a>
             </div>
         </div>
     </div>
